ce3fc155f5eba16a7deb7e2eda27e9cfa2788d2e
Abbas W <abbas.code786@gmail.com>
2025-06-17 06:32:55 +0530

    minor fiex

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// connect to Da
+// connect to Database
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server is running on port:", PORT);
