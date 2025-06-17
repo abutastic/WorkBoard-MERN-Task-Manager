660be556a5ca093a8eebec153cfe2e9428d5ba61
Abbas W <abbas.code786@gmail.com>
2025-06-17 06:34:15 +0530

    minor bug fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// connect to Database before
+// connect to Database before st
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server is running on port:", PORT);
