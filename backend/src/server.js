837196cda4991a43f7e7fd0c0ad1ab1b2fa6590d
Abbas W <abbas.code786@gmail.com>
2025-06-24 16:54:36 +0530

    feature added

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// connect to Database before starting the server
+// connect to Database before starting the server 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server is running on port:", PORT);
